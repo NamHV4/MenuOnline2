d54bfa0256ecd0c142da109b0500ce5f1881d5f3
Minh Quang <minhquang0094@gmail.com>
2018-01-30 12:26:12 +0700

    [minhtq] Add splash screen with login form

---
+++ app/src/main/java/com/codedao/menuonline/SplashScreen.java
@@ -0,0 +1,71 @@
+package com.codedao.menuonline;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.transition.Explode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SplashScreen extends AppCompatActivity{
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btGo;
+    private CardView cv;
+    private FloatingActionButton fab;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        initView();
+        setListener();
+    }
+
+
+
+    private void initView() {
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        btGo = findViewById(R.id.bt_go);
+        cv = findViewById(R.id.cv);
+        fab = findViewById(R.id.fab);
+    }
+
+    private void setListener() {
+        btGo.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                Explode explode = new Explode();
+                explode.setDuration(500);
+
+                getWindow().setExitTransition(explode);
+                getWindow().setEnterTransition(explode);
+                ActivityOptionsCompat oc2 = ActivityOptionsCompat.makeSceneTransitionAnimation(SplashScreen.this);
+                Intent i2 = new Intent(SplashScreen.this,LoginSuccessActivity.class);
+
+                startActivity(i2, oc2.toBundle());
+            }
+        });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                getWindow().setExitTransition(null);
+                getWindow().setEnterTransition(null);
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this, fab, fab.getTransitionName());
+                startActivity(new Intent(SplashScreen.this, RegisterActivity.class), options.toBundle());
+            }
+        });
+    }
+}
