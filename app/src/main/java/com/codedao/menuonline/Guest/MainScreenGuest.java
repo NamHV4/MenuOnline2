73d003fcf1762bb7228b511f7220dd3fd543d4ca
Minh Quang <minhquang0094@gmail.com>
2018-01-30 18:04:50 +0700

    [minhtq] Add FancyGIFDialog and custom action depend on context

--- app/src/main/java/com/codedao/menuonline/Guest/MainScreenGuest.java
+++ app/src/main/java/com/codedao/menuonline/Guest/MainScreenGuest.java
@@ -10,46 +10,68 @@ import com.codedao.menuonline.Adapter.RecyclerviewTableAdapter;
 import com.codedao.menuonline.Interface.RecyclerviewTableItemClick;
 import com.codedao.menuonline.Model.Table;
 import com.codedao.menuonline.R;
+import com.shashank.sony.fancygifdialoglib.FancyGifDialog;
+import com.shashank.sony.fancygifdialoglib.FancyGifDialogListener;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-public class MainScreenGuest extends AppCompatActivity implements RecyclerviewTableItemClick{
+public class MainScreenGuest extends AppCompatActivity implements RecyclerviewTableItemClick {
     RecyclerView rcvTable;
     ArrayList<Table> listTables;
     RecyclerviewTableAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen_guest2);
         getSupportActionBar().hide();
         initView();
-        listTables=new ArrayList<>();
-        rcvTable.setLayoutManager(new GridLayoutManager(this,3));
-        for (int i = 0; i <15 ; i++) {
-            int a=randomMaxChair();
-            listTables.add(new Table(i+1,randomEmptyChair(a),a));
+        listTables = new ArrayList<>();
+        rcvTable.setLayoutManager(new GridLayoutManager(this, 3));
+        for (int i = 0; i < 15; i++) {
+            int a = randomMaxChair();
+            listTables.add(new Table(i + 1, randomEmptyChair(a), a));
         }
-        adapter=new RecyclerviewTableAdapter(listTables,this,this);
+        adapter = new RecyclerviewTableAdapter(listTables, this, this);
         rcvTable.setAdapter(adapter);
     }
 
     private void initView() {
-        rcvTable=findViewById(R.id.rcvTable);
+        rcvTable = findViewById(R.id.rcvTable);
     }
-    private int randomMaxChair(){
-        Random r=new Random();
-        return r.nextInt(5)+2;
+
+    private int randomMaxChair() {
+        Random r = new Random();
+        return r.nextInt(5) + 2;
 
     }
-    private int randomEmptyChair(int x){
-        Random r=new Random();
+
+    private int randomEmptyChair(int x) {
+        Random r = new Random();
         return r.nextInt(x);
 
     }
 
     @Override
     public void onItemCick(int position) {
-        Toast.makeText(this, "Empty chair "+listTables.get(position).getEmtyChair(), Toast.LENGTH_SHORT).show();
+        int emptyChair = listTables.get(position).getEmtyChair();
+        final FancyGifDialog.Builder fancyGifDialog = new FancyGifDialog.Builder(this).setGifResource(R.drawable.mountain)
+                .setMessage("Empty chair: " + emptyChair).setNegativeBtnText("Cancel")
+                .OnNegativeClicked(new FancyGifDialogListener() {
+                    @Override
+                    public void OnClick() {
+
+                    }
+                })
+                .OnPositiveClicked(new FancyGifDialogListener() {
+                    @Override
+                    public void OnClick() {
+
+                    }
+                })
+                .isCancellable(false);
+        fancyGifDialog.setPositiveBtnText(emptyChair == 0 ? "Check out" : "Check in");
+        fancyGifDialog.build();
     }
 }
